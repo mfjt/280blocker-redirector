2d279b272e01cbf4ab920b0ebc466e467cadf4a0
oɹıɥɐsɐɯ ɐʇıɾnɟ <mfjt@users.noreply.github.com>
2020-11-03 04:05:28 +0900

    Implementation

--- src/handler.ts
+++ src/handler.ts
@@ -1,3 +1,25 @@
+const BASE_URI: string = 'https://280blocker.net/files/'
+
 export async function handleRequest(request: Request): Promise<Response> {
-  return new Response(`request method: ${request.method}`)
+  let filename: string = request.url.slice(request.url.lastIndexOf('/') + 1)
+  if (!filename) {
+    return new Response(null, {
+      status: 400,
+    })
+  }
+
+  let extensionIndex: number = filename.lastIndexOf('.')
+  let extension: string = extensionIndex >= 0
+    ? filename.slice(extensionIndex)
+    : ''
+  let baseName: string = extension
+    ? filename.slice(0, -extension.length)
+    : filename
+
+  let date: Date = new Date()
+  var year: string = date.getFullYear().toString()
+  let month: string = ('0' + (date.getMonth() + 1)).slice(-2)
+  let redirectTo: string = BASE_URI + baseName + '_' + year + month + extension
+
+  return Response.redirect(redirectTo, 303)
 }
--- test/handler.ts
+++ test/handler.ts
@@ -1,23 +1,40 @@
 import { expect } from 'chai'
+import { beforeEach } from 'mocha'
 import { handleRequest } from '../src/handler'
+import * as sinon from 'sinon'
 
 describe('handler returns response with request method', () => {
-  const methods = [
-    'GET',
-    'HEAD',
-    'POST',
-    'PUT',
-    'DELETE',
-    'CONNECT',
-    'OPTIONS',
-    'TRACE',
-    'PATCH',
+  var sandbox: sinon.SinonSandbox
+
+  beforeEach(function () {
+    sandbox = sinon.createSandbox()
+    sandbox.useFakeTimers(new Date(2020, 11 - 1, 1).getTime())
+  })
+
+  afterEach(function () {
+    sandbox.restore();
+  })
+
+  const URI_BASE = 'https://280blocker.net/files/'
+  const FILENAME_ARRAYS = [
+    ['280blocker_adblock.txt', '280blocker_adblock_202011.txt'],
+    ['280blocker_1blocker.1blockpkg', '280blocker_1blocker_202011.1blockpkg'],
+    ['280blocker_adblock', '280blocker_adblock_202011'],
   ]
-  methods.forEach((method) => {
-    it(method, async () => {
-      const result = await handleRequest(new Request('/', { method }))
-      const text = await result.text()
-      expect(text).to.include(method)
+
+  FILENAME_ARRAYS.forEach((filename) => {
+    it('/' + filename[0], async () => {
+      let response = await handleRequest(new Request('/' + filename[0]))
+      let status = response.status
+      let location = response.headers.get('location')
+      expect(status).equal(303)
+      expect(location).equal(URI_BASE + filename[1])
     })
+  })
+
+  it('/', async () => {
+    let response = await handleRequest(new Request('/'))
+    let status = response.status
+    expect(status).equal(400)
   })
 })
